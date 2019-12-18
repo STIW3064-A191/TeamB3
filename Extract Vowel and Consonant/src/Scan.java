2f669cffff51a9373912c4c0c6d96b6f91bc8626
Shakir Zufayri Osman <47938666+ShakirZufayri95@users.noreply.github.com>
2019-12-18 16:09:16 +0800

    Update Scan.java

--- Extract Vowel and Consonant/src/Scan.java
+++ Extract Vowel and Consonant/src/Scan.java
@@ -8,11 +8,11 @@ public class Scan {
         Consonant c = new Consonant();
         Scanner sc = new Scanner(System.in);  // Create a Scanner object
         System.out.print("Enter a string : ");
-        String s = sc.nextLine();
+        String s = sc.nextLine(); // Read user input
         System.out.println("Vowels in a string '" + s + "' are :");
-        v.vowels(s);
+        v.vowels(s); // send input to method
 
         System.out.println("Conconant in a string '" + s + "' are :");
-        c.consonant(s);
+        c.consonant(s); // send input to method
     }
 }
